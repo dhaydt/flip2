7da2db0a75356efcd28e9cb6e561cd52b08343bd
dhaydt <1992dayat@gmail.com>
2022-08-28 13:23:34 +0700

    fix app name

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
     <div class="container">
-        <a class="navbar-brand" href="#">FlipBooks App</a>
+        <a class="navbar-brand" href="#">StarterHolic</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
